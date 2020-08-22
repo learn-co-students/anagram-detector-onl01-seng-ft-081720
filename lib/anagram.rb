a222da0264ac400e6523e074b4b8e26a560db4ed
Eli Cusic <tntitans31@gmail.com>
2020-08-22 11:23:37 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(challenge)
+        #breakdown @word into an alphabetized string
+        alphabetical = @word.split('').sort.join
+        
+        #break down the challenge argument into an array of strings (words)
+        array = challenge.to_s.gsub(/[^a-zA-Z]/, " ").split(" ")
+        #create seperate array that evaluates truthiness against our alphebitized @word
+        mapped = array.map{|word| word.split('').sort.join == alphabetical}
+
+        #cycle through the mapped array, and push each truthy array item with the matching index
+        final = []
+        i= 0
+        while i < mapped.length
+        if mapped[i]
+            final << array[i]
+        end
+        i+=1
+        end
+        final
+    end
+end
+  
+
+
