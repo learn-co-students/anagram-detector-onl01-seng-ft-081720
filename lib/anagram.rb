6b5e0276e30b0ad0c56d5be35c7d650137886df5
Tabatha Slatton <valbatha@gmail.com>
2020-08-23 08:24:18 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+ attr_accessor :word
+    
+ def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        word_array = @word.split("").sort
+        matches = []
+        anagrams.each do |ana_word|
+            anagram_array=ana_word.split("").sort
+            matches << ana_word if anagram_array == word_array
+        end
+        matches
+    end
+end
