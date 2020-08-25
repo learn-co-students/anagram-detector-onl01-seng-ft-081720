2241d652181e1a894cf3867c1a9ea869204069fc
atBeatty <adam.t.beatt@gmail.com>
2020-08-25 07:50:09 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
 # Your code goes here!
+
+class Anagram
+attr_accessor :match_array
+
+
+    def initialize(word)
+        @word = word
+
+    end
+
+    def match(array)
+        @match_array = []
+
+        array.each do |word|
+            if @word.split("").sort == word.split("").sort
+                @match_array << word
+            end                        
+        end
+        @match_array
+    end
+
+    def detect(array)
+        
+
+    end
+
+
+
+
+
+    
+
+end
