76d84d68775dc98770b138a547f66812c6d6993a
Richard Kurwitz <kurwitz3@aol.com>
2020-08-29 20:13:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+attr_accessor :word
+def initialize(word)
+ @word = word
+ end 
+ 
+ def match(array)
+   array.keep_if{|words|word.split('').sort == words.split('').sort}
+ 
+end 
+
+
+end 
